bf27d289354f20c83c4118d346a644336d22affc
Faris Natour-D <natourfaris@gmail.com>
2020-01-24 23:56:38 -0500

    Add figure generation

---
+++ src/visualization/create_graphs.py
@@ -0,0 +1,30 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+subgraph = nx.Graph()
+
+subgraph_players = ['Sidney Crosby','Phil Kessel','Dion Phaneuf','Erik Karlsson']
+game_names = ['2016020379a','2016020379h']
+
+subgraph.add_nodes_from(subgraph_players,bipartite='players')
+subgraph.add_nodes_from(game_names,bipartite='games')
+
+subgraph.add_edges_from([('Sidney Crosby','2016020379h'),('Phil Kessel','2016020379h'),
+                     ('Dion Phaneuf','2016020379a'),('Erik Karlsson','2016020379a')])
+
+proj_graph = nx.algorithms.bipartite.projection.projected_graph(subgraph,subgraph_players)
+
+figure = plt.figure(figsize=(10,6))
+ax = plt.subplot(111)
+top = nx.drawing.layout.bipartite_layout(subgraph,subgraph_players)
+top = {k:(np.array([0.75,v[1]]) if k in subgraph_players else v) for k,v in top.items()}
+nx.draw(subgraph,with_labels=True,pos=top)
+plt.savefig('../figures/bipartite.png')
+plt.close()
+
+figure = plt.figure(figsize=(10,6))
+ax = plt.subplot(111)
+nx.draw_spring(proj_graph,with_labels=True)
+plt.savefig('../figures/bipartite2.png')
+plt.close()
